2b374d84b30f66b37376268bba0cf4fd2aac2550
Maciej Dzwonczyk <maciejdzwonczyk@gmail.com>
2022-12-11 21:17:30 +0100

    zadanie-20-coś nie tak

---
+++ kurs-część-2/zadanie-20-js/js/script.js
@@ -0,0 +1,22 @@
+// alert('cześć! ');
+
+// // var/*zapis przestarzały*/zastaąpiony let, const
+
+// const color = 'niebieski';
+// // typ string
+// let age = '24';
+// // typ namber
+// // console.log(age);
+
+// const name = 'Lisa';
+
+// console.log('cześć, mam na imie ${name} i mam ${age} lata. ');
+
+
+const name = 'Maciek'
+
+const eat = 'carry'
+
+let age = '27'
+
+console.log('Cześć, jestem ${name}, lubie jeść ${eat}. Mam ${age} lat.')
