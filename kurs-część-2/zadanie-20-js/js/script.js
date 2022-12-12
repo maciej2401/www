eded745f31d570b88cbfbceae6e40971e9a8224d
Maciej Dzwonczyk <maciejdzwonczyk@gmail.com>
2022-12-12 19:04:54 +0100

    zadanie-20-poprawny-zapis

--- kurs-część-2/zadanie-20-js/js/script.js
+++ kurs-część-2/zadanie-20-js/js/script.js
@@ -12,11 +12,12 @@
 
 // console.log('cześć, mam na imie ${name} i mam ${age} lata. ');
 
+const name = 'Maciek';
 
-const name = 'Maciek'
+const eat = 'carry';
 
-const eat = 'carry'
+let age = '27';
 
-let age = '27'
-
-console.log('Cześć, jestem ${name}, lubie jeść ${eat}. Mam ${age} lat.');
+console.log(
+	'Cześć, jestem ' + name + ', lubie jeść ' + eat + '. Mam ' + age + ' lat.'
+);
