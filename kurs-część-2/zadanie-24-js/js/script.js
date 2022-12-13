91d8bf4bd043328fd21a2ec5741974ee1d41db22
Maciej Dzwonczyk <maciejdzwonczyk@gmail.com>
2022-12-13 16:02:42 +0100

    zadanie-24-operatory-przypisania

---
+++ kurs-część-2/zadanie-24-js/js/script.js
@@ -0,0 +1,21 @@
+let x = 8;
+let y = 4;
+
+x = x + y;
+console.log(x);
+x += y; //zapis skrucony+y
+console.log(x);
+
+x = x - y; //x=16
+x -= y; //zapis skrucony
+
+console.log(x);
+
+x = x * y;//x *= y;
+console.log(x);
+
+x = x / y;//x /= y;
+console.log(x);
+
+x = x % y;//x %= y;
+console.log(x);
