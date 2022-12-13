4c2e464801489a1b33616451678ac45af4c4624c
Maciej Dzwonczyk <maciejdzwonczyk@gmail.com>
2022-12-13 13:55:31 +0100

    zadanie-21-js-complite

---
+++ kurs-część-2/zadanie-21-js/js/script.js
@@ -0,0 +1,7 @@
+const FavColor = 'red'
+const Meal = 'schabowy'
+const FavDrink = 'pepsi'
+const Numbers = 123
+const Name = 'Maciek'
+const Age = 27
+console.log(`Ceść jestem ${Name} i mam ${Age} lat`);
