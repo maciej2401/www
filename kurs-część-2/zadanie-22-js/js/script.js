3b18f02c58e963247cd1cd2689707fa044f1f1e4
Maciej Dzwonczyk <maciejdzwonczyk@gmail.com>
2022-12-13 15:32:40 +0100

    zadanie-22-js-typy-danych

---
+++ kurs-część-2/zadanie-22-js/js/script.js
@@ -0,0 +1,23 @@
+/*********************************/
+//typy danych
+/*********************************/
+
+// ***Typy proste***
+
+// STRING-'cześć'
+const name = 'list'
+
+//NAMBER - 13
+const age =137.5
+
+//BOOLEAN - true/fals
+const isTrue = true
+
+//NULL & UNDEFINED
+const test = null
+let test2
+console.log(test);
+console.log(test2);
+// ***Typ złożony***
+
+// OBJECT
