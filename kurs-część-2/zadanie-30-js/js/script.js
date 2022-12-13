e2d30e939c8da5c28429ca5954a4446dc9db580c
Maciej Dzwonczyk <maciejdzwonczyk@gmail.com>
2022-12-14 00:00:41 +0100

    zadanie-30-js-function

---
+++ kurs-część-2/zadanie-30-js/js/script.js
@@ -0,0 +1,26 @@
+function test() {
+	console.log(`Moja pierwsza funkcja`);
+}
+
+test();
+
+function addNumbers(x, y) {
+	console.log(x, y);
+	console.log(x + y);	
+	console.log(x - y);
+	console.log(x / y);
+	console.log(x * y);
+}
+
+addNumbers(5, 10)
+
+
+
+function showName(name){console.log(`Podane imie to: ${name}`);}
+
+showName(`Klaudiusz`)
+
+
+function age(age){console.log(`Mam ${age} lat`);}
+
+age(15)
