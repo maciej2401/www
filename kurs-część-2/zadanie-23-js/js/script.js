fe8e89c16a95de24bbd68b878a9feaeed753ae80
Maciej Dzwonczyk <maciejdzwonczyk@gmail.com>
2022-12-13 15:50:24 +0100

    zadanie-23-js-zabawa-liczbami-podstawy

---
+++ kurs-część-2/zadanie-23-js/js/script.js
@@ -0,0 +1,25 @@
+const add = 5 + 9
+const add1 = 5 * 3
+console.log(`wynik dodawania ${add}`);
+console.log(`wynik mnożenia ${add1}`);
+console.log(`wynik mnożenia ${add * add1}`)
+const namber = 3
+console.log(`wynik dzielenia ${add * add1 / namber}`)
+console.log(`wynik odejmowania ${add1 - namber}`);
+console.log(`${1+2*5*0.3}`)
+
+const modulo =15 % 7
+console.log(`wynik modulo ${modulo}`);
+//modulo daje reszte z dzielenia do sprawdzania parzystości
+
+//++ inkrementacja
+//-- dekrementacja
+
+let num = 4
+num++
+num++
+num++
+num++
+console.log(num)
+num--
+console.log(num);
