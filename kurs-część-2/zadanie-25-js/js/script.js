5d746dd4da02e153e5dab095615656fefe3c1627
Maciej Dzwonczyk <maciejdzwonczyk@gmail.com>
2022-12-13 16:17:43 +0100

    zadanie-25-js-operatory-porównania

---
+++ kurs-część-2/zadanie-25-js/js/script.js
@@ -0,0 +1,22 @@
+const x = 8;
+const y = '8';
+const z = 9;
+
+//== sprawdzony, czy coś jest RÓWNE(sama wartość)
+//=== sprawdzony, czy coś jest RÓWNE (wartość i typ danych)
+
+//!= sprawdzony, czy coś hest RUŻNE (sama wartość)
+//!== sprawdzony, czy coś hest RUŻNE (wartość i typ danych)
+
+// > większe niż...
+// < mniejsze niż...
+
+// >= większe lub równe
+// <= mniejsze lub równe
+
+
+if (x === y) {
+	console.log(1);
+} else {
+	console.log(0);
+}
