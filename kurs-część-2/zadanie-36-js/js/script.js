ae046688500b041d05a567115f80770c5072a518
Maciej Dzwonczyk <maciejdzwonczyk@gmail.com>
2022-12-19 19:49:43 +0100

    zadanie-36-js

---
+++ kurs-część-2/zadanie-36-js/js/script.js
@@ -0,0 +1,4 @@
+const sizeUp = document.querySelector(`.size-up`);
+const sizeDown = document.querySelector(`.size-down`);
+const color = document.querySelector(`.color`);
+const p = document.querySelector(`p`);
