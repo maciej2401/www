d45a32c074db59645938a0f83b77570cc26d2101
Maciej Dzwonczyk <maciejdzwonczyk@gmail.com>
2022-12-20 20:30:27 +0100

    zadanie-36-js

--- kurs-część-2/zadanie-36-js/js/script.js
+++ kurs-część-2/zadanie-36-js/js/script.js
@@ -2,3 +2,24 @@ const sizeUp = document.querySelector(`.size-up`);
 const sizeDown = document.querySelector(`.size-down`);
 const color = document.querySelector(`.color`);
 const p = document.querySelector(`p`);
+
+let fontSize = 20;
+
+function bigText() {
+	fontSize += 5;
+	p.style.fontSize = fontSize + `px`;
+}
+
+function smallText() {
+	fontSize -= 5;
+	p.style.fontSize = fontSize + `px`;
+}
+
+function gold() {
+	p.style.color = `gold`;
+}
+
+sizeUp.addEventListener(`click`, bigText);
+sizeDown.addEventListener(`click`, smallText);
+
+color.addEventListener(`click`, gold);
